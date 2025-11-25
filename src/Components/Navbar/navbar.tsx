f106d283534ec870d5d3c5069a990259e54bdfb8
Wil-dev <mehdididou7@icloud.com>
2025-11-25 22:41:08 +0100

    feat: ajout d'un menu mobile dans la barre de navigation et mise à jour de la configuration de turbopack

--- src/Components/Navbar/navbar.tsx
+++ src/Components/Navbar/navbar.tsx
@@ -132,6 +132,44 @@ export default function Navbar() {
           </button>
 				</div>
 			</div>
+
+      {/* mobile menu */}
+      <div className={`lg:hidden bg-(--white) border-t border-gray-400 overflow-hidden transition-all duration-500 ${mobileMenuOpen ? "max-h-[700px] opacity-100 py-4" : "max-h-0 opacity-0 py-0"}`}>
+            <div className="px-[8%] space-y-3">
+              {navLinks.map((link) => (
+                <div key={link.label} className="border border-gray-700/50 rounded-lg overflow-hidden">
+                  {link.dropdown ? (
+                    <>
+                      <button
+                        onClick={() => toggleDropdown(link.label)}
+                        className="w-full flex items-center justify-between px-4 py-3 text-left text-(--text) font-medium hover:text-(--prim) transition">
+                          {link.label} <i className={`ri-arrow-down-s-line transition-transform duration-300 ${openDropdown[link.label] ? "rotate-180" : ""}`}></i>
+                        </button>
+                        <div className={`pl-6 pr-4 bg-gray-800/10 border-t border-gray-700/40 transition-all duration-500 ${openDropdown[link.label] ? "max-h-[300px] opacity-100 py-2" : "max-h-0 opacity-0 py-0"}`}>
+                          {link.dropdown.map((item) => (
+                            <Link
+                              key={item.label}
+                              href={item.href}
+                              className="block py-2 font-semibold hover:text-(--prim) transition border-b border-gray-700/60"
+                              onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+                              {item.label}
+                            </Link>
+                          ))}
+                        </div>
+                    </>
+                  ) : (
+                    <Link
+                      href={link.href}
+                      className="block px-4 py-3 text-(--text) hover:text-(--prim) transition font-medium"
+                      onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+                      {link.label}
+                    </Link>
+                  )}
+                </div>
+              ))}
+            </div>
+      </div>
+
 		</div>
 	);
 }
