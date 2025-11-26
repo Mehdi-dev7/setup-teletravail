1740720fef7b5645085e28950eabce4559883dca
Wil-dev <mehdididou7@icloud.com>
2025-11-26 20:09:34 +0100

    feat: ajout d'un bouton "Get Our Services" et d'une image de contenu dans la section Hero

--- src/app/UI-Components/Index/Hero/page.tsx
+++ src/app/UI-Components/Index/Hero/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import HeroImg from "@/../public/Hero-img.png";
-import HeroImg2 from "@/../public/Hero-img2.png";
+import HeroImg2 from "@/../public/hero-img2.jpg";
 import Link from "next/link";
 
 export default function Hero() {
@@ -17,6 +17,17 @@ export default function Hero() {
               <span className="hero-span border border-gray-400 GolosText font-bold px-5 py-2 rounded-full">
                 Trusted Design Partner
               </span>
+              <h1 className="text-6xl lg:text-8xl CalSans my-5">Find Your Inspiration <span className="text-(--prim)">Interior Design</span></h1>
+              <p className="text-gray-600 w-full md:w-[60%]">Whether is your home, office or a commercial project,we are always dedicated your vision to life.</p>
+              <button className="mt-9 border border-gray-500/50 px-5 py-3 rounded-full GolosText font-semibold hover:bg-(--prim) hover:text-white hover:border-transparent transition-all duration-300 cursor-pointer group">
+                <Link href="/UI-Components/Pages/Services">
+                  Get Our Services <i className="bi bi-arrow-right ps-1"></i>
+                </Link>
+              </button>
+              <div className="hero-content-img absolute top-10 right-10 cursor-pointer hidden md:block">
+                <Image src={HeroImg2} alt="HeroImg2" className="rounded-full" />
+                <i className="bi bi-play-fill"></i>
+              </div>
             </div>
           </div>
         </div>
