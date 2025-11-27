04ab4f9ad367b3b969d41e331949400974542b59
Wil-dev <mehdididou7@icloud.com>
2025-11-27 14:17:13 +0100

    Add SVG files for partner graphics: partner5.svg and partner6.svg

--- src/app/UI-Components/Index/Hero/page.tsx
+++ src/app/UI-Components/Index/Hero/page.tsx
@@ -1,37 +1,90 @@
 "use client";
 
 import Image from "next/image";
-import HeroImg from "@/../public/Hero-img.png";
+import HeroImg from "@/../public/Hero-img.jpg";
 import HeroImg2 from "@/../public/hero-img2.jpg";
 import Link from "next/link";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import partner1 from "@/../public/partner1.svg";
+import partner2 from "@/../public/partner2.svg";
+import partner3 from "@/../public/partner3.svg";
+import partner4 from "@/../public/partner4.svg";
+import partner5 from "@/../public/partner5.svg";
+import partner6 from "@/../public/partner6.svg";
+
+const Partners = [partner1, partner2, partner3, partner4, partner5, partner6];
 
 export default function Hero() {
-  return (
-    <div className="hero">
-      <div className="hero-bg-elm"></div>
-      <div className="hero-bg-elm2"></div>
-      <div className="w-full px-[8%] lg:px-[12%] py-10">
-        <div className="flex flex-col lg:flex-row gap-5 justify-between items-center">
-          <div className="w-full lg:w-1/2 relative">
-            <div className="">
-              <span className="hero-span border border-gray-400 GolosText font-bold px-5 py-2 rounded-full">
-                Trusted Design Partner
-              </span>
-              <h1 className="text-6xl lg:text-8xl CalSans my-5">Find Your Inspiration <span className="text-(--prim)">Interior Design</span></h1>
-              <p className="text-gray-600 w-full md:w-[60%]">Whether is your home, office or a commercial project,we are always dedicated your vision to life.</p>
-              <button className="mt-9 border border-gray-500/50 px-5 py-3 rounded-full GolosText font-semibold hover:bg-(--prim) hover:text-white hover:border-transparent transition-all duration-300 cursor-pointer group">
-                <Link href="/UI-Components/Pages/Services">
-                  Get Our Services <i className="bi bi-arrow-right ps-1"></i>
-                </Link>
-              </button>
-              <div className="hero-content-img absolute top-10 right-10 cursor-pointer hidden md:block">
-                <Image src={HeroImg2} alt="HeroImg2" className="rounded-full" />
-                <i className="bi bi-play-fill"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<>
+			<div className="hero">
+				<div className="hero-bg-elm"></div>
+				<div className="hero-bg-elm2"></div>
+				<div className="w-full px-[8%] lg:px-[12%] py-10">
+					<div className="flex flex-col lg:flex-row gap-5 justify-between items-center">
+						<div className="w-full lg:w-1/2 relative">
+							<div className="">
+								<span className="hero-span border border-gray-400 GolosText font-bold px-5 py-2 rounded-full">
+									Trusted Design Partner
+								</span>
+								<h1 className="text-6xl lg:text-8xl CalSans my-5">
+									Find Your Inspiration{" "}
+									<span className="text-(--prim)">Interior Design</span>
+								</h1>
+								<p className="text-gray-600 w-full md:w-[60%]">
+									Whether is your home, office or a commercial project,we are
+									always dedicated your vision to life.
+								</p>
+								<button className="mt-9 border border-gray-500/50 px-5 py-3 rounded-full GolosText font-semibold hover:bg-(--prim) hover:text-white hover:border-transparent transition-all duration-300 cursor-pointer group">
+									<Link href="/UI-Components/Pages/Services">
+										Get Our Services <i className="bi bi-arrow-right ps-1"></i>
+									</Link>
+								</button>
+								<div className="hero-content-img absolute top-10 right-10 cursor-pointer hidden md:block">
+									<Image
+										src={HeroImg2}
+										alt="HeroImg2"
+										className="rounded-full"
+									/>
+									<i className="bi bi-play-fill"></i>
+								</div>
+							</div>
+						</div>
+						<div className="w-full lg:w-1/2 z-10">
+							<div className="hero-image">
+								<Image
+									src={HeroImg}
+									alt="HeroImg"
+									className="w-full h-full rounded-2xl"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="w-full px-[8%] lg:px-[12%] pb-10">
+				<Swiper
+					slidesPerView={5}
+					spaceBetween={30}
+					loop={true}
+					autoplay={{ delay: 1500 }}
+					modules={[Autoplay]}
+					breakpoints={{
+						1200: { slidesPerView: 5 },
+						991: { slidesPerView: 4 },
+						575: { slidesPerView: 2 },
+						0: { slidesPerView: 2 },
+					}}
+          className="partner-swiper"
+				>
+          {Partners.map((partner, index) => (
+            <SwiperSlide key={index}>
+              <Image src={partner} alt="PartnerImage" className="partner-img" />
+            </SwiperSlide> ))}
+        </Swiper>
+			</div>
+		</>
+	);
 }
