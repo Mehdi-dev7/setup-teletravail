e94561c52108b4e7f613723a597d3f9536c67f90
Wil-dev <mehdididou7@icloud.com>
2025-12-25 22:20:59 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/UI-Components/Projects/[id]/page.tsx
+++ src/app/UI-Components/Projects/[id]/page.tsx
@@ -1,5 +1,55 @@
-export default function page() {
-  return (
-    <div>page</div>
-  )
+"use client";
+
+
+import Link from "next/link";
+import ProjectsData from "../../../../JsonData/Projects.json";
+import { useParams } from "next/navigation";
+
+export default function ProjectDetails() {
+
+
+  const { id }=useParams();
+  const project = ProjectsData.find((p) => p.id === Number(id));
+
+  if (!project) {
+    return (
+      <div className="section-bg text-white flex flex-col">
+        <h1 className="text-5xl md:text-6xl GolosText mt-15">Project not found</h1>
+        <div className="flex items-center gap-5 mt-3">
+          <Link href="/UI-Components/Projects" className="hover:text-(--prim) transition-all duration-300 mt-5 border border-gray-300 px-4 py-2 rounded hover:bg-white">Back to Projects</Link>
+        </div>
+      </div>
+    )
+  }
+
+
+
+	return (
+		<>
+			<div className="section-bg text-white flex flex-col">
+				<h1 className="text-6xl md:text-8xl GolosText mt-15">
+					Projects Details
+				</h1>
+				<div className="flex items-center text-xl mt-3">
+					<Link
+						href="/"
+						className="hover:text-(--prim) transition-all duration-300"
+					>
+						Home
+					</Link>
+					<i className="ri-arrow-right-wide-fill mt-1"></i>
+					<Link
+						href="/UI-Components/Projects"
+						className="hover:text-(--prim) transition-all duration-300"
+					>
+						Projects
+					</Link>
+          <i className="ri-arrow-right-wide-fill mt-1"></i>
+					<h2 className="GolosText text-white">{project.title}</h2>
+				</div>
+			</div>
+
+    {/* Project details content */}
+		</>
+	);
 }
